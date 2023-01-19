398579ac8b3151548e9775fac14b150f5b3da344
Nathan Woodward <nathanrichardwoodward@gmail.com>
2023-01-19 10:28:17 +0000

    Copy files explicitly

--- bin/generate.sh
+++ bin/generate.sh
@@ -2,4 +2,27 @@
 # Generate the site
 #
 
-( cd src && zola build -o .. ) || exit 1
+# Build the site
+( cd src && zola build ) || exit 1
+
+# Delete built files
+rm -f 404.html
+rm -f index.html
+rm -f robots.txt
+rm -f sitemap.xml
+rm -f style.css
+rm -rf about/
+rm -rf hpmfp/
+rm -rf images/
+rm -rf posts/
+
+# Copy built files
+cp 404.html .
+cp index.html .
+cp robots.txt .
+cp sitemap.xml .
+cp style.css .
+cp -r about/ .
+cp -r hpmfp/ .
+cp -r images/ .
+cp -r posts/ .
